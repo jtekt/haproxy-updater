91b3f1616c9f04fd43dab7e2f8801d7989e64b5c
Maxime Moreillon <m.moreillon@gmail.com>
2025-03-19 08:49:53 +0900

    loadfromcluster

--- k8s.ts
+++ k8s.ts
@@ -3,8 +3,8 @@ import * as k8s from "@kubernetes/client-node";
 export async function getNodes() {
   const kc = new k8s.KubeConfig();
 
-  kc.loadFromDefault();
-  // kc.loadFromCluster();
+  // kc.loadFromDefault();
+  kc.loadFromCluster();
 
   const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
   const { items } = await k8sApi.listNode();
